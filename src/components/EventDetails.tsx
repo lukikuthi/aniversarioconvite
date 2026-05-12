97e96ceb4cb95ebf5b97e8820f344216900c9e24
lukikuthi <lucaskikuthi@gmail.com>
2026-05-12 00:18:55 -0300

    Configurar GitHub Pages

--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -46,7 +46,7 @@ const eventInfo: InfoItem[] = [
     icon: Backpack,
     label: "O que levar",
     value: "Animação e sede!",
-    detail: "Levar traje para banho",
+    detail: "Levar traje para banho - Toalha (Caso for dormir: Roupa de Cama)",
   },
   {
     icon: AlertCircle,
